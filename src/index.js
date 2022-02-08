54d3333da97085fc123e6ef57f54f9052ac6e633
Alex Adamovic <alexanderadamovic@gmail.com>
2022-02-08 11:24:00 -0800

    refactor giphy application to use promises and static methods
    Co-authored-by: Ryan Bass <theryanbass@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,72 +2,59 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import SearchGiphy from './search-giphy';
 
 $(document).ready(function(){
+
+
   $("#gifSearch").click(function(){
     const searchTerm = $('#gif-search').val();
     $('#gif-search').val("");
     $(".showGifs").html("");
-    let request = new XMLHttpRequest();
-    const url =  `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${searchTerm}&limit=25&offset=0&rating=g&lang=en`;
-
-    request.onreadystatechange = function(){
-      if(this.readyState === 4 && this.status === 200){
-        const response = JSON.parse(this.responseText);
-        getElements(response);
+    let url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${searchTerm}&limit=25&offset=0&rating=g&lang=en`;
+    let promise = SearchGiphy.searchPics(url);
+    promise.then(function(response) {
+      const body = JSON.parse(response);
+      if (body.data.length === 0) {
+        $(".showGifs").text("No results for your search.");
       }
-    };
-
-    request.open("GET", url, true);
-    request.send();
-
-    function getElements(response){
-      for(let i = 0; i < response.data.length; i ++){
-        $(".showGifs").append(`<img src="${response.data[i].images.fixed_height_small.url}">`);
+      for(let i = 0; i < body.data.length; i ++){
+        $(".showGifs").append(`<img src="${body.data[i].images.fixed_height_small.url}">`);
       }
-    }
+    }, function(error) {
+      $(".showGifs").text(`Here's what the error came back as: ${error}`);
+    });
   });
+
   $("#gifTrending").click(function() {
     $(".showGifs").html("");
-    let request = new XMLHttpRequest();
     let url = `https://api.giphy.com/v1/gifs/trending?api_key=${process.env.API_KEY}&limit=25&rating=g`;
-
-    request.onreadystatechange = function(){
-      if(this.readyState === 4 && this.status === 200){
-        const response = JSON.parse(this.responseText);
-        getElements(response);
+    let promise = SearchGiphy.searchPics(url);
+    promise.then(function(response) {
+      const body = JSON.parse(response);
+      if (body.data.length === 0) {
+        $(".showGifs").text("Nothing is trending for some reason.");
       }
-    };
-
-    request.open("GET", url, true);
-    request.send();
-
-    function getElements(response){
-      for(let i = 0; i < response.data.length; i ++){
-        $(".showGifs").append(`<img src="${response.data[i].images.fixed_height_small.url}">`);
+      for(let i = 0; i < body.data.length; i ++){
+        $(".showGifs").append(`<img src="${body.data[i].images.fixed_height_small.url}">`);
       }
-    }
+    }, function(error) {
+      $(".showGifs").text(`Here's what the error came back as: ${error}`);
+    });
   });
 
   $("#gifRandom").click(function() {
     $(".showGifs").html("");
-    console.log("click");
-    let request = new XMLHttpRequest();
     let url = `https://api.giphy.com/v1/gifs/random?api_key=${process.env.API_KEY}&tag=&rating=g`;
-
-    request.onreadystatechange = function(){
-      if(this.readyState === 4 && this.status === 200){
-        const response = JSON.parse(this.responseText);
-        getElements(response);
+    let promise = SearchGiphy.searchPics(url);
+    promise.then(function(response) {
+      const body = JSON.parse(response);
+      if (body.data.length === 0) {
+        $(".showGifs").text("Nothing is Random.");
       }
-    };
-
-    request.open("GET", url, true);
-    request.send();
-
-    function getElements(response){
-      $(".showGifs").append(`<img src="${response.data.images.fixed_height_small.url}">`);
-    }
+      $(".showGifs").append(`<img src="${body.data.images.fixed_height_small.url}">`);
+    }, function(error) {
+      $(".showGifs").text(`Here's what the error came back as: ${error}`);
+    });
   });
 });
-
