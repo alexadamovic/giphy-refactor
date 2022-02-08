bc4c9cf35fb18e92006b8f1e06a649e011ab69ca
Alex Adamovic <alexanderadamovic@gmail.com>
2022-02-08 11:26:19 -0800

    remove commented out code
    Co-authored-by: Ryan Bass <theryanbass@gmail.com>

--- src/search-giphy.js
+++ src/search-giphy.js
@@ -2,7 +2,6 @@ export default class SearchGiphy {
   static searchPics(url){
     return new Promise(function(resolve, reject){
       let request = new XMLHttpRequest();
-      // const url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${searchTerm}&limit=25&offset=0&rating=g&lang=en`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
